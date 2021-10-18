c06aceba952b14fe6b1a5ba0b5bcbcc9789ede14
daduJadhav <87080275+daduJadhav@users.noreply.github.com>
2021-10-18 18:47:06 +0530

    commit 1

---
+++ class_05.py
@@ -0,0 +1,14 @@
+# import TextBlob
+from textblob import TextBlob
+
+a = TextBlob("Campk12 is a good compny and alays valule ttheir employeees.")
+
+# using TextBlob.correct() method
+a = a.correct()
+
+print(a)
+
+# Spelling Corrector
+# ///////////////////////////////////////
+
+
